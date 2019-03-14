1c2dc20a4afaddbb8eef65c60e4972d75d9b5df5
Mateus Bezerra <mocbezerra@gmail.com>
2019-03-14 01:29:27 -0300

    adicionando tudo

---
+++ main.py
@@ -0,0 +1,52 @@
+from flask import Flask, jsonify
+from flask import request
+from flask import render_template
+from flask import redirect
+
+from flask.ext.sqlalchemy import SQLAlchemy
+
+from flask.ext.heroku import Heroku
+
+app = Flask(__name__)
+heroku = Heroku(app)
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+  __tablename__ = "persons"
+  id = db.Column(db.Integer, primary_key=True)
+  nome = db.Column(db.String(255), unique=True)
+  key = db.Column(db.String(255), unique=True)
+
+  def __init__(self, nome, key):
+    self.nome = nome  
+    self.key = key  
+
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+  if request.method == 'POST':
+    keys = request.form.getlist('cbox')
+    text = request.form.getlist('tarea')
+
+    print(keys)
+    print(text)
+    
+    return redirect('/')
+  else:
+    p1 = Person("mateus", "abcdefghijlm")
+    p2 = Person("italo", "vovomalfalda")
+    lp = []
+    lp.append(p1)
+    lp.append(p2)
+
+    return render_template('main.html', pessoas=lp)
+
+@app.route("/adicionar", methods=['POST'])
+def add_pessoa():
+  p = Person(request.json['nome'], request.json['key'])
+  db.session.add(p)
+  db.session.commit()
+
+  return jsonify({'status': 'ok'}), 201
+
+if __name__ == '__main__':
+  app.run(debug=True,host='0.0.0.0')
